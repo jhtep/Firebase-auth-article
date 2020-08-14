05d4fe3fb1cbbd6612ccc3f8ef0160de3072329e
jhtep <john@epvisual.com>
2020-08-14 16:30:25 -0400

    fire-project

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,17 @@
-import firebase from "firebase/app";
-import "firebase/auth";
-import "firebase/firestore";
-import { functions } from "firebase";
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+import { functions } from 'firebase';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA9WZB5N6ekNxyN3yGaUwjuBilvXItUv38",
-  authDomain: "fir-auth-article.firebaseapp.com",
-  databaseURL: "https://fir-auth-article.firebaseio.com",
-  projectId: "fir-auth-article",
-  storageBucket: "fir-auth-article.appspot.com",
-  messagingSenderId: "774252759419",
-  appId: "1:774252759419:web:e014ddfa3553a4832a15de",
-  measurementId: "G-77Z5WJ0SET"
+  apiKey: 'AIzaSyBPbQDoRUGzRg9fvCoLCAeXrqJxFZqUP8I',
+  authDomain: 'fire-project-661ba.firebaseapp.com',
+  databaseURL: 'https://fire-project-661ba.firebaseio.com',
+  projectId: 'fire-project-661ba',
+  storageBucket: 'fire-project-661ba.appspot.com',
+  messagingSenderId: '258780333028',
+  appId: '1:258780333028:web:6398be3ad1d3c7aae0fdf1',
+  measurementId: 'G-DJ80M765YL',
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
@@ -37,10 +37,10 @@ export const generateUserDocument = async (user, additionalData) => {
         displayName,
         email,
         photoURL,
-        ...additionalData
+        ...additionalData,
       });
     } catch (error) {
-      console.error("Error creating user document", error);
+      console.error('Error creating user document', error);
     }
   }
   return getUserDocument(user.uid);
@@ -53,9 +53,9 @@ const getUserDocument = async uid => {
 
     return {
       uid,
-      ...userDocument.data()
+      ...userDocument.data(),
     };
   } catch (error) {
-    console.error("Error fetching user", error);
+    console.error('Error fetching user', error);
   }
 };
