4eb4a2f175b6445df04d73f8e562b7e9f6b0b031
jhtep <john@epvisual.com>
2020-08-15 00:21:38 -0400

    format

--- src/providers/UserProvider.jsx
+++ src/providers/UserProvider.jsx
@@ -1,23 +1,18 @@
-import React, { Component, createContext } from "react";
-import { auth, generateUserDocument } from "../firebase";
-
+import React, { Component, createContext } from 'react';
+import { auth, generateUserDocument } from '../firebase';
 
 export const UserContext = createContext({ user: null });
 
 class UserProvider extends Component {
   state = {
-    user: null
+    user: null,
   };
 
-  
-  
   componentDidMount = async () => {
     auth.onAuthStateChanged(async userAuth => {
       const user = await generateUserDocument(userAuth);
       this.setState({ user });
     });
-
-
   };
 
   render() {
